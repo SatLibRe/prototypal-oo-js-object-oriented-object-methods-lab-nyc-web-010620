c5a95eb63241361a208c20a9a4d54d07e3f798c9
Miles Higbie <miles.higbie@gmail.com>
2020-03-02 02:43:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 class BoardMember {
   constructor(name,homeState,training){
     this.name = name; 
-    this.homeState = homestate;
+    this.homeState = homeState;
     this.training = training;
   }
 }
